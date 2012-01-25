02af1923363c1b37a6bbcd0834c894b05a6dde7d
Pete Muir <pmuir@bleepbleep.org.uk>
2012-01-25 13:38:25 +0000

    Use getters and setters so forge doesn't get confused

--- src/main/java/org/jboss/jdf/example/ticketmonster/model/Address.java
+++ src/main/java/org/jboss/jdf/example/ticketmonster/model/Address.java
@@ -7,10 +7,33 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class Address {
-    
-    public String street;
-    
-    public String city;
-    
-    public String country;
+
+   private String street;
+   private String city;
+   private String country;
+
+   public String getStreet() {
+      return street;
+   }
+
+   public void setStreet(String street) {
+      this.street = street;
+   }
+
+   public String getCity() {
+      return city;
+   }
+
+   public void setCity(String city) {
+      this.city = city;
+   }
+
+   public String getCountry() {
+      return country;
+   }
+
+   public void setCountry(String country) {
+      this.country = country;
+   }
+
 }
