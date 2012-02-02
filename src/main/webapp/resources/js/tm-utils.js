d8462749a2ca1529da22f1d379f4cf9888b2334c
Marius Bogoevici <marius.bogoevici@gmail.com>
2012-02-01 23:57:31 -0500

    Housekeeping * extracting utilities in a separate JavaCript file * removed unused files * Upgrade to backbone.js 0.9

---
+++ src/main/webapp/resources/js/tm-utils.js
@@ -0,0 +1,19 @@
+
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+Date.prototype.toPrettyString = function () {
+    return dayNames[this.getDay()] + " " +
+            this.getDate() + " " +
+            monthNames[this.getMonth()] + " " +
+            this.getFullYear() + " @ " +
+            this.getHours().toZeroPaddedString(2) + ":" +
+            this.getMinutes().toZeroPaddedString(2);
+}
+
+Number.prototype.toZeroPaddedString = function(digits) {
+    val = this + "";
+    while (val.length < digits) val = "0" + val;
+    return val;
+}
